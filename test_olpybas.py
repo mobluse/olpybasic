1fb21325f56c5527c88f40c117db6ef984649fce
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-02-20 03:32:40 +0100

    Update test_olpybas.py

--- test_olpybas.py
+++ test_olpybas.py
@@ -1,4 +1,4 @@
 # Should be pasted in OLPyBASIC.
-print("4nd line in program should be pasted in OLPyBASIC.")
+print("4th line in program should be pasted in OLPyBASIC.")
 P=print
 P('10 print("OLPyBASIC")');P('20 print("uPython")');P('30 x=30');P('40 print(x)');P('45 gosub(90)');P('50 x+=1');P('60 if x<=40:goto(40)');P('70 stop()');P('90 print(11*x)');P('99 ret()')
